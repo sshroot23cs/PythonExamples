577203bd88f2880395c1c9da0a7563c90dd00112
sshroot23cs <sshroot23cs@gmail.com>
2024-03-03 11:03:35 +0530

    parallel execution

--- Selenium/src/utilities/customLogger.py
+++ Selenium/src/utilities/customLogger.py
@@ -1,4 +1,6 @@
 import logging
+import queue
+from logging.handlers import QueueHandler, QueueListener
 import os
 
 class CustomLogger:
@@ -9,11 +11,17 @@ class CustomLogger:
 
         log_file_name = os.path.join(os.path.dirname(__file__), "../../logs", "automation.log")
         file_handler = logging.FileHandler(log_file_name, mode='w')
-        formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+        formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(threadName)s: %(name)s : %(message)s')
         file_handler.setFormatter(formatter)
-
+        # queue handler to handle logs from multiple threads
+        log_queue = queue.Queue(-1)
+        queue_handler = QueueHandler(log_queue)
+        self.logger.addHandler(queue_handler)
         self.logger.addHandler(file_handler)
-
+        listener = QueueListener(log_queue, file_handler)
+        listener.start()
+        self.logger.warning('Look out!')
+        listener.stop()
 
     def get_logger(self):
         return self.logger
